5a392a05553e9683ba2566331b420d0c1638ff59
Andrei <aceris_d@yahoo.com>
2015-04-04 21:15:26 +0300

    ready for copy-paste in hr

--- Solve me first/solve_me_first.py
+++ Solve me first/solve_me_first.py
@@ -1,9 +1,9 @@
 def solveMeFirst(a,b):
   return a+b
 
-# num1 = int(input())
-# num2 = int(input())
-num1 = int(5)
-num2 = int(4)
+num1 = int(input())
+num2 = int(input())
+# num1 = int(5)
+# num2 = int(4)
 res = solveMeFirst(num1,num2)
 print (res)
