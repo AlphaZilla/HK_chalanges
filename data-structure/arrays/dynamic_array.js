83f9888179f274ecc502f2900d77f753ec6cd568
Andrei-WPc <aceris_d@yahoo.com>
2020-02-21 14:32:01 +0200

    weird challenge - not solving

---
+++ data-structure/arrays/dynamic_array.js
@@ -0,0 +1,16 @@
+// weird request from HR
+// pass this challenge
+let firstInput = 2;
+let secondInput = [[1, 0, 5], [1, 1, 7], [1, 0, 3], [2, 1, 0], [2, 1, 1]];
+let secondInputLength = secondInput.length; // 5
+let lastAnswer = 0;
+
+console.log(secondInputLength);
+
+function dynamicArray(n, queries) {
+  // Write your code here
+  console.log(n);
+  console.log(queries);
+}
+
+dynamicArray(firstInput, secondInput);
